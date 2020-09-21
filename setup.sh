17e62c69dab7f5b42b95af24fb730cd415da0a96
Bhagesh Hunakunti <hunakunti.basavraj@learner.manipal.edu>
2020-09-22 00:52:41 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,13 +1,10 @@
 mkdir -p ~/.streamlit/
-
 echo "\
 [general]\n\
-email = \"hunakuntibhagesh@gmail.com"\n\
+email = \"hunakuntibhagesh@gmail.com\"\n\
 " > ~/.streamlit/credentials.toml
-
-echo “\
+echo "\
 [server]\n\
 headless = true\n\
 enableCORS=false\n\
 port = $PORT\n\
-“ > ~/.streamlit/config.toml
